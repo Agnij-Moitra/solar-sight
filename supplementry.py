d7d90e522a21423f91d6530c5fd0bc4eeb09c046
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2022-07-09 13:49:42 +0530

    update supplementry.py

--- supplementry.py
+++ supplementry.py
@@ -1,3 +1,18 @@
 import pickle
+import pandas as pd
+
 with open('model_pickle', "rb") as f:
     imported_model = pickle.load(f)
+KEYS = ['wind-speed',
+        'average-wind-speed-(period)',
+        'average-pressure-(period)',
+        'humidity',
+        'wind-direction']
+    
+def get_energy_preds(wind_direction, wind_speed, humidity, average_wind_speed, average_pressure):
+    zipped = list(zip([wind_direction], [wind_speed], [humidity], [average_wind_speed], [average_pressure]))
+    df_ = pd.DataFrame(zipped, columns=KEYS)
+    return imported_model.predict(df_)
+
+if __name__ == "__main__":
+    print(get_energy_preds(30, 12.1, 93, 8.0, 29.99))
