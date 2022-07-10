b8b03e74a5bbc4404112d22d4cac197949b4af5d
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2022-07-10 12:53:56 +0530

    update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def index():
-    return render_template("index.html")
+    return render_template("./index.html")
 
 
 @app.route('/api/<location>', methods = ['GET'])
