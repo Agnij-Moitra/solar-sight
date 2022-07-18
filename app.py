139a0ed73407e3a940821630d04f8f3e3b22b19f
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2022-07-18 23:20:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from supplementry import get_preds
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
-def index():
-    return render_template("./index.html")
+def index(): return render_template("./index.html")
 
 @app.route('/api/', methods = ['GET'])
-def api():
-    return jsonify({'estimatedElectricity': float(get_preds(str(request.args.to_dict().get('place')))), 'unit': 'watts'})
+def api(): return jsonify({'estimatedElectricity': float(get_preds(str(request.args.to_dict().get('place')))), 'unit': 'watts'})
   
 if __name__ == '__main__':
     app.run(debug = True)
